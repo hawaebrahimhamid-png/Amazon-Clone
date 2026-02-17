ac752f9843a5e20e2d9f434daf23ae110bcd10b5
hawa ebrahim <hawaebrahimhamid@gmail.com>
2026-02-17 15:32:00 +0800

    fix netlify build script

--- Amazon_icon_svg/src/Api/Axios.js
+++ Amazon_icon_svg/src/Api/Axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-c34b5/us-central1/api",
+  //   baseURL: "http://127.0.0.1:5001/clone-c34b5/us-central1/api",
+  //   deployment on renderMatches.com
+  baseURL: "https://amazon-api-deploy-1hl1.onrender.com",
 });
 
 export default axiosInstance;
